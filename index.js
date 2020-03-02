bede546ed3aaf1e62cefb91e1cd3955aecbdd582
Imad Ahmed <erimadahmad@gmail.com>
2020-03-02 17:42:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// alert("Hello All from js file")
+// prompt("Please confirm your age.")
+// console.log("Form console.log")
+
+var myFirstVar = "JavaScript";
+var myFirstNum = 10;
+var myFirstB = false;
+var test = undefined;
+var test2 = null;
+
+alert(myFirstVar);
+console.log(myFirstNum * 20)
+
+// console.log(typeof "Hello World")
+// console.log(typeof 65)
+// console.log(typeof false)
+// console.log(typeof undefined)
+// console.log(typeof null)
+
+console.log(typeof myFirstB)
+
+var arr = [1, 5, 6, 10]
+
+var obj = {
+    name: "Imad",
+    company: "wavelabs"
+}
+
+console.log(obj.name +" works for "+ obj.company)
